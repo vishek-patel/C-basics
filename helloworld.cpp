cf78875acb321b08d0492ce4006f95cd7c40882d
Vishek Patel <vishek.12001921@lpu.in>
2023-01-13 21:26:07 +0530

    added a helloworld program

---
+++ helloworld.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+
+	cout<<"Hello world!";
+	
+	return 0;
+	
+}
+
+//   //(two backslashes) are used to denote a comment in c++
+
+//  /* */ --> this is used to denote a multi line comment in c++
+
+
+/*
+	This is a 
+	mulit line
+	comment
+
+*/
+
+
+
